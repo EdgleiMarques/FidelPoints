2641bc95f7d6fc2b97bbb11c0c8565640ffd1812
Edglei Marques <edgleitab@gmail.com>
2023-09-22 10:07:21 -0300

    Adicionar arquivos de projeto.

---
+++ FidelPoints/Models/Client.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FidelPoints.Models
+{
+    [Table("Clients")]
+    public class Client
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public int Point { get; set; }
+        public ICollection<Order> Orders { get; set; }
+
+
+    }
+}
---
+++ FidelPoints/Models/Order.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FidelPoints.Models
+{
+    public class Order
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int SellerId { get; set; }
+        public Seller Sellers { get; set; }
+        [Required]
+        public int ProductId { get; set; }
+        public ICollection<Product> Products { get; set; }
+        [Required]
+        public int ClientId { get; set; }
+        public Client Clients { get; set; }
+
+
+
+
+    }
+}
