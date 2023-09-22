c0fb41288a4ba82b45ac12e754aabd05bb4ac266
Edglei Marques <edgleitab@gmail.com>
2023-09-22 17:27:22 -0300

    Adicionado CRUD dos modelos Clients, Product e Order

---
+++ FidelPoints/Controllers/ClientsController.cs
@@ -0,0 +1,59 @@
+﻿using FidelPoints.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FidelPoints.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public ClientsController(AppDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult> Getall()
+        {
+            var client = await _context.Clients.ToListAsync();
+            return Ok(client);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(Client client)
+        {
+            await _context.AddAsync(client);
+            await _context.SaveChangesAsync();
+            return Ok(client);
+        }
+        [HttpGet("id")]
+        public async Task<ActionResult> GetById(int id)
+        {
+            var client = await _context.Clients.FindAsync(id);
+            if (client == null) return BadRequest(new { message = "Id não encontrado" });
+            return Ok(client);
+        }
+        [HttpPut("id")]
+        public async Task<ActionResult> Update(int id, Client client)
+        {
+            if (client.Id != id) return BadRequest();
+            if (await _context.Products.AsNoTracking()
+                    .FirstOrDefaultAsync(c => c.Id == id) == null) return NotFound();
+            _context.Clients.Update(client);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpDelete("id")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var client = await _context.Clients.FindAsync(id);
+            if (client == null) return NotFound();
+            _context.Clients.Remove(client);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
---
+++ FidelPoints/Controllers/OrdersController.cs
@@ -0,0 +1,59 @@
+﻿using FidelPoints.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FidelPoints.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public OrdersController(AppDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult> Getall()
+        {
+            var client = await _context.Orders.ToListAsync();
+            return Ok(client);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(Order order)
+        {
+            await _context.AddAsync(order);
+            await _context.SaveChangesAsync();
+            return Ok(order);
+        }
+        [HttpGet("id")]
+        public async Task<ActionResult> GetById(int id)
+        {
+            var order = await _context.Orders.FindAsync(id);
+            if (order == null) return BadRequest(new { message = "Id não encontrado" });
+            return Ok(order);
+        }
+        [HttpPut("id")]
+        public async Task<ActionResult> Update(int id, Order order)
+        {
+            if (order.Id != id) return BadRequest();
+            if (await _context.Orders.AsNoTracking()
+                    .FirstOrDefaultAsync(c => c.Id == id) == null) return NotFound();
+            _context.Orders.Update(order);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpDelete("id")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var order = await _context.Orders.FindAsync(id);
+            if (order == null) return NotFound();
+            _context.Orders.Remove(order);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
